7cfa6c88b063da570c4edc5383b8dcb05a6380aa
deveezy <deveezysl@gmail.com>
2022-12-25 20:55:05 +0300

    upd

--- nvim/lua/ezy/plugins-setup.lua
+++ nvim/lua/ezy/plugins-setup.lua
@@ -38,6 +38,22 @@ return require('packer').startup(function(use)
   -- fuzzy finding w/ telescope
   use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
   use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+
+  -- autocompletion
+  use("hrsh7th/nvim-cmp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+
+  -- snippets
+  use("L3MON4D3/LuaSnip")
+  use("saadparwaiz1/cmp_luasnip")
+  use("rafamadriz/friendly-snippets")
+
+  -- utility
+  use("windwp/nvim-autopairs")
+  use('rmagatti/auto-session')
+  use("lukas-reineke/indent-blankline.nvim")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if packer_bootstrap then
