a01ecef1f98061ff0e8b41548e9178db65cc8a3b
deveezy <deveezysl@gmail.com>
2022-12-25 18:11:26 +0300

    config

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -Ux EDITOR vim
+set -U fish_greeting
+fish_add_path /home/deveezy/.local/bin/poetry
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
