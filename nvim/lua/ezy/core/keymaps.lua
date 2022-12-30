3ed68a1c23749fec7e426bfa39edebf353940d6b
deveezy <deveezysl@gmail.com>
2022-12-31 01:17:23 +0300

    upd

--- nvim/lua/ezy/core/keymaps.lua
+++ nvim/lua/ezy/core/keymaps.lua
@@ -3,6 +3,17 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 vim.keymap.set("n", "<leader>+", "<C-a>")
 vim.keymap.set("n", "<leader>-", "<C-x>")
 
+-- handy shortcut for escaping from terminal mode
+vim.keymap.set("t", "<Esc>", "<C-Bslash><C-n>")
+
+-- Copy/Paste to/from system clipboard
+-- check system clipboard exists ---> :echo has('clipboard')
+vim.keymap.set("n", "<c-c>", '"*y :let @+=@*<CR>', {noremap=true, silent=true})
+vim.keymap.set("n", "<c-v>", '"+p', {noremap=true, silent=true})
+
+vim.keymap.set("v", "<c-c>", '"*y :let @+=@*<CR>', {noremap=true, silent=true})
+vim.keymap.set("v", "<c-v>", '"+p', {noremap=true, silent=true})
+
 vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window veritically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally 
 vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
@@ -13,27 +24,32 @@ vim.keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 vim.keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 vim.keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to prev tab
 
+-- move lines
 vim.keymap.set("v", "<C-A-j>", ":m'>+<CR>gv=gv")
 vim.keymap.set("v", "<C-A-k>", ":m-2<CR>gv=gv")
+
+-- handy shortcuts
 vim.keymap.set("n", "H", "^")
 vim.keymap.set("n", "L", "$")
 vim.keymap.set("v", "H", "^")
 vim.keymap.set("v", "L", "$")
--- plugin keymaps:
 
+-- plugin keymaps:
 -- vim-maximizer
 vim.keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- nvim-tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 
+-- telescope
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
 vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>") -- find string in current working directory as you type
 vim.keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string under cursor in current working directory
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
+vim.keymap.set("n", "<leader>pp", "<cmd>Telescope projects<cr>")
 
+-- browser
 vim.keymap.set("n", "<m-o>", "<cmd>BrowseBookmarks<cr>")
 vim.keymap.set("n", "<m-i>", "<cmd>BrowseInputSearch<cr>")
-vim.keymap.set("t", "<Esc>", "<C-Bslash><C-n>")
-vim.keymap.set("n", "<leader>pp", "<cmd>Telescope projects<cr>")
+
