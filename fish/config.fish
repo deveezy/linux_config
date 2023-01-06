6c223a9caa463d129f4348adc1f649ddc782c61f
deveezy <deveezysl@gmail.com>
2023-01-07 00:58:10 +0300

    upd

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -Ux EDITOR vim
+set -Ux EDITOR nvim
 set -U fish_greeting
 fish_add_path /home/deveezy/.local/bin/poetry
 
