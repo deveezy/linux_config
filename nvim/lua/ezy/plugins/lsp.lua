339373476003995e4912cbaf7a2f219be2099ea3
444444444444444444444 <deveezysl@gmail.com>
2023-01-30 12:03:48 +0300

    upd

--- nvim/lua/ezy/plugins/lsp.lua
+++ nvim/lua/ezy/plugins/lsp.lua
@@ -84,20 +84,25 @@ require'lspconfig'.clangd.setup ({
   capabilities = capabilities
 })  
 
-vim.cmd([[
+-- need to install neocmakelsp
+local configs = require("lspconfig.configs")
+local nvim_lsp = require("lspconfig")
+if not configs.neocmake then
+    configs.neocmake = {
+        default_config = {
+            cmd = { "neocmakelsp", "--stdio" },
+            filetypes = { "cmake" },
+            root_dir = function(fname)
+                return nvim_lsp.util.find_git_ancestor(fname)
+            end,
+            single_file_support = true,-- suggested
+            on_attach = on_attach -- on_attach is the on_attach function you defined
+        }
+    }
+    nvim_lsp.neocmake.setup({})
+end
 
-if executable('cmake-language-server')
-  au User lsp_setup call lsp#register_server({
-  \ 'name': 'cmake',
-  \ 'cmd': {server_info->['cmake-language-server']},
-  \ 'root_uri': {server_info->lsp#utils#path_to_uri(lsp#utils#find_nearest_parent_file_directory(lsp#utils#get_buffer_path(), 'build/'))},
-  \ 'whitelist': ['cmake'],
-  \ 'initialization_options': {
-  \   'buildDirectory': 'build',
-  \ }
-  \})
-endif
-]])
+
 -- require("clangd_extensions").setup {
 --     server = {
 --         -- options to pass to nvim-lspconfig
--- nvim/lua/ezy/plugins/null-ls.lua
+++ nvim/lua/ezy/plugins/null-ls.lua
@@ -3,9 +3,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.cmake_format,
-        null_ls.builtins.diagnostics.clang_check,
+        null_ls.builtins.formatting.clang_format.with{filetypes = { "c", "cpp", "cs", "java", "cuda", "h", "hpp", "cxx" }},
+        null_ls.builtins.formatting.cmake_format.with{filetypes = {"cmake", "txt"}},
+        null_ls.builtins.diagnostics.clang_check.with{filetypes = { "c", "cpp", "cxx", "hpp", "h" }},
         null_ls.builtins.diagnostics.cmake_lint,
     },
 })
