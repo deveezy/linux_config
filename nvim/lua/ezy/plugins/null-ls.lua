74aa2143701ac301a287e77ec4a2016dba77b352
deveezy <deveezysl@gmail.com>
2023-01-30 20:54:12 +0300

    upd

--- nvim/lua/ezy/plugins/null-ls.lua
+++ nvim/lua/ezy/plugins/null-ls.lua
@@ -5,7 +5,6 @@ null_ls.setup({
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format.with{filetypes = { "c", "cpp", "cs", "java", "cuda", "h", "hpp", "cxx" }},
         null_ls.builtins.formatting.cmake_format.with{filetypes = {"cmake", "txt"}},
-        null_ls.builtins.diagnostics.cpplint.with{filetypes = { "c", "cpp", "cxx", "hpp", "h" }},
         null_ls.builtins.diagnostics.cmake_lint,
     },
 })
